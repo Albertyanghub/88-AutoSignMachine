69ed649095a72a85070725302032619d2053d908
Albertyanghub <78647982+Albertyanghub@users.noreply.github.com>
2021-03-23 17:11:18 +0800

    Update unicom.js

--- commands/tasks/unicom/unicom.js
+++ commands/tasks/unicom/unicom.js
@@ -203,7 +203,7 @@ var start = async (params) => {
 
   // 首页-游戏-娱乐中心-天天领取3G流量包
   await scheduler.regTask('dailygameflow', async (request) => {
-    await require('./producGame').allgames(request, options)
+    await require('./producGame').doGameFlowTask(request, options)
   }, taskOption)
 
   // 首页-积分查询-游戏任务
